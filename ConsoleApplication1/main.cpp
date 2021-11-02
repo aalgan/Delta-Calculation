bc3629fde5f90d752fc3c953b48130013a8988e1
algan <61619135+aalgan@users.noreply.github.com>
2021-11-02 21:27:40 +0100

    Add files via upload

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	cout << "Provide a: " << endl;
+	float a;
+	cin >> a;
+	cout << "Provide b: " << endl;
+	float b;
+	cin >> b;
+	cout << "Provide c: " << endl;
+	float c;
+	cin >> c;
+
+	// Δ = b^2 - 4 * a * c
+
+	float delta = (b * b) - 4 * a * c;
+	float xzero = (-b) / (2 * a);
+	float xone = ((-b - sqrt(delta)) / (2 * a));
+	float xtwo = ((-b + sqrt(delta)) / (2 * a));
+
+	if (delta > 0) {
+		cout << "Delta equals: " << delta << endl;
+		cout << "x1 equals: " << xone << endl;
+		cout << "x2 equals: " << xtwo << endl;
+	}
+	else if (delta < 0) {
+		cout << "Delta equals less than 0." << endl;
+		cout << "There is no solution." << endl;
+	}
+	else if (delta == 0) {
+		cout << "Delta equals: 0." << endl;
+		cout << xzero << endl;
+	}
+	system("pause");
+}
